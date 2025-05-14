489c6c24b63ac4e3b4540c89bf544c2ea2048acd
MA\maaya <yokoo.maaya@compass-japan.com>
2025-05-14 15:55:21 +0900

    気温追加

--- src/components/renderTenki.jsx
+++ src/components/renderTenki.jsx
@@ -2,16 +2,19 @@ export const RenderTenki = (props) => {
     const {tenkiData} = props;
     console.log('tenkiData props',props);
     let renderText = "";
+    let renderKion = "";
     if(tenkiData.data) {
         console.log("renderTenki.jsx", tenkiData.data[0].publishingOffice);
         const getTenkiData = tenkiData.data[0].timeSeries[0].areas[0].weathers[0].split("　");
         renderText = getTenkiData[0];
+        renderKion = tenkiData.data[0].timeSeries[2].areas[0].temps[0];
     } else {
         renderText = "検索に失敗しました。";
     }
     return(
         <>
         <h1>{!tenkiData.data && !tenkiData.error ? "" : renderText}</h1>
+        <h2>{renderKion}°</h2>
         </>
     );
 }
