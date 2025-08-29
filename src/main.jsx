715f31801e8eef01c9f301689a3b7860f64aaa56
MA\maaya <yokoo.maaya@compass-japan.com>
2025-08-29 16:21:23 +0900

    github pages 表示されない問題を修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // ← ここを変更！
 // import './index.css'               // 削除
   
 createRoot(document.getElementById('root')).render(
   <StrictMode>  
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </StrictMode> 
 )
